1495330cfa0bad04150bb788484bce5a875d6eac
Per Unneberg <per.unneberg@scilifelab.se>
2011-10-18 18:39:02 +0200

    project based analysis -- major update
    - renamed scripts -> have prefix project
    - removed unnecessary code in scripts
    - changed command line arguments to follow automated_initial_analysis.py

--- nextgen/tests/test_project_exome_pipeline.py
+++ nextgen/tests/test_project_exome_pipeline.py
@@ -43,10 +43,11 @@ class SampleBasedAnalysisTest(unittest.TestCase):
     """Setup a sample based scilife analysis
     """
     def setUp(self):
-        self.fcid = "110106_FC70BUKAAXX"
         self.file_dir = os.path.join(os.path.dirname(__file__))
-        self.proj_dir = os.path.join(self.file_dir, "projects")
-        self.fcdir = os.path.join(os.path.dirname(__file__), "test_automated_output")
+        self.fc_dir = os.path.join(self.file_dir, "110106_FC70BUKAAXX")
+        self.proj_dir = os.path.join(self.file_dir, "projects", "j_doe_00_01")
+        ##self.fcdir = os.path.join(os.path.dirname(__file__), "test_automated_output")
+        self.run_info = os.path.join(self.fc_dir, "run_info.yaml")
         self.archive_base_dir  = os.path.join(self.file_dir)
         self.analysis_base_dir = os.path.join(self.file_dir)
 
@@ -65,16 +66,17 @@ class SampleBasedAnalysisTest(unittest.TestCase):
         post_process["analysis"]["base_dir"] = os.path.join(self.analysis_base_dir)
         post_process["algorithm"]["snpcall"] = "true"
         post_process["algorithm"]["dbsnp"] = os.path.join("data", "genomes", "hg19", "variation", "dbsnp_132.vcf")
-        with open(os.path.join(self.analysis_base_dir, self.fcid, "post_process.yaml"), "w") as fh:
+        with open(os.path.join(self.fc_dir, "post_process.yaml"), "w") as fh:
             yaml.dump(post_process, stream=fh)
 
     def _deliver_data(self):
         print "Delivering data"
-        cl = ["sample_delivery.py",
-              os.path.join(self.file_dir, self.fcid, "post_process.yaml"),
-              self.fcid, "j_doe_00_01",
+        cl = ["project_analysis_setup.py",
+              os.path.join(self.fc_dir, "post_process.yaml"),
+              self.fc_dir, self.proj_dir,
+              self.run_info,
+              "--data_prefix=intermediate/nobackup",
               "--project_desc=%s" % "J.Doe_00_01",
-              "--project_base_dir=%s" % self.proj_dir,
               "--flowcell_alias=20000101A_hiseq2000"]
         subprocess.check_call(cl)
         print "Finished delivering data..."
@@ -84,9 +86,9 @@ class SampleBasedAnalysisTest(unittest.TestCase):
         with make_workdir():
             print "Going to deliver data"
             self._deliver_data()
-            cl = ["exome_pipeline.py",
-                  os.path.join(self.analysis_base_dir, self.fcid, "post_process.yaml"),
-                  os.path.join(self.proj_dir, "j_doe_00_01", "intermediate", "nobackup", "110106_FC70BUKAAXX"),
-                  os.path.join(self.proj_dir, "j_doe_00_01", "data", "nobackup", "20000101A_hiseq2000", "project_run_info.yaml"),
-                  "--project_dir=%s" % (self.proj_dir)]
+            cl = ["project_exome_pipeline.py",
+                  os.path.join(self.fc_dir, "post_process.yaml"),
+                  os.path.join(self.proj_dir, "intermediate", "nobackup", "110106_FC70BUKAAXX"),
+                  os.path.join(self.proj_dir, "intermediate", "nobackup", "20000101A_hiseq2000", "project_run_info.yaml"),
+                  "--project_dir=%s" %(self.proj_dir)]
             subprocess.check_call(cl)
