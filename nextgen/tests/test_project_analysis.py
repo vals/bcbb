1ee0b7f19f1ce606007afd164a9d5327cd499698
Per Unneberg <per.unneberg@scilifelab.se>
2011-10-18 18:39:02 +0200

    project based analysis -- major update
    - renamed scripts -> have prefix project
    - removed unnecessary code in scripts
    - changed command line arguments to follow automated_initial_analysis.py

--- nextgen/tests/test_project_analysis.py
+++ nextgen/tests/test_project_analysis.py
@@ -33,14 +33,15 @@ class SampleDeliveryTest(unittest.TestCase):
     """Deliver samples from bcbio-processed data to John Doe"""
     
     def setUp(self):
-        self.fcid = "110106_FC70BUKAAXX"
         self.file_dir = os.path.dirname(os.path.abspath(__file__))
-        self.proj_dir = os.path.join(self.file_dir, "projects")
-        self.fcdir = os.path.join(os.path.dirname(__file__), "test_automated_output")
+        self.fc_dir   = os.path.join(self.file_dir, "110106_FC70BUKAAXX")
+        self.proj_dir = os.path.join(self.file_dir, "projects", "j_doe_00_01")
+        self.run_info = os.path.join(self.fc_dir, "run_info.yaml")
+        ##self.fc_orig_dir = os.path.join(os.path.dirname(__file__), "test_automated_output")
         self.archive_base_dir  = os.path.join(self.file_dir)
         self.analysis_base_dir  = os.path.join(self.file_dir)
 
-        self.post_process = os.path.join(self.analysis_base_dir, self.fcid, "post_process.yaml")
+        self.post_process = os.path.join(self.analysis_base_dir, self.fc_dir, "post_process.yaml")
         if os.path.exists(self.proj_dir):
             shutil.rmtree(self.proj_dir)
         if not os.path.exists(self.proj_dir):
@@ -61,40 +62,41 @@ class SampleDeliveryTest(unittest.TestCase):
         """Test dry run: don't do anything"""
         self.setUp()
         with make_workdir():
-            cl = ["sample_delivery.py",
+            cl = ["project_analysis_setup.py",
                   self.post_process,
-                  self.fcid, "j_doe_00_01",
-                  "--project_base_dir=%s" % self.proj_dir, 
+                  self.fc_dir, self.proj_dir,
+                  self.run_info,
                   "--project_desc=%s" % "J.Doe_00_01",
                   "--dry_run"]
             subprocess.check_call(cl)
 
     def test_2_write_run_info_only(self):
 	"""Test writing of pruned run info file"""
-        cl = ["sample_delivery.py",
+        cl = ["project_analysis_setup.py",
               self.post_process,
-              self.fcid, "j_doe_00_01",
-              "--project_base_dir=%s" % self.proj_dir, 
+              self.fc_dir, self.proj_dir,
+              self.run_info,
               "--project_desc=%s" % "J.Doe_00_01",
               "--only_install_run_info"]
         subprocess.check_call(cl)
 
     def test_3_deliver_data_copy(self):
         """Test data delivery by default method copy"""
-        cl = ["sample_delivery.py",
+        cl = ["project_analysis_setup.py",
               self.post_process,
-              self.fcid, "j_doe_00_01",
-              "--project_base_dir=%s" % self.proj_dir, 
+              self.fc_dir,  self.proj_dir,
+              self.run_info,
               "--project_desc=%s" % "J.Doe_00_01"]
         subprocess.check_call(cl)
 
     def test_4_deliver_data(self):
         """Test data delivery by moving"""
         with make_workdir(link=False):
-            cl = ["sample_delivery.py",
+            cl = ["project_analysis_setup.py",
                   self.post_process,
-                  self.fcid, "j_doe_00_01",
-                  "--project_base_dir=%s" % self.proj_dir, 
+                  self.fc_dir,  self.proj_dir,
+                  self.run_info,
+                  "--data_prefix=nobackup",
                   "--project_desc=%s" % "J.Doe_00_01", "--move_data"]
             subprocess.check_call(cl)
 
