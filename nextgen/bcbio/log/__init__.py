a7d958f8f4306ef900df8476c7313577a027a4f2
b97pla <pontus.larsson@scilifelab.se>
2012-02-07 12:49:12 +0100

    multiple notification recipients

--- nextgen/bcbio/log/__init__.py
+++ nextgen/bcbio/log/__init__.py
@@ -20,8 +20,9 @@ def create_log_handler(config, log_name):
         smtp_host = config.get("smtp_host",None)
         smtp_port = config.get("smtp_port",25)
         if smtp_host is not None: smtp_host = [smtp_host,smtp_port]
-            
-        handler = logbook.MailHandler(email, [email], server_addr=smtp_host,  
+        
+        email = email.split(",")
+        handler = logbook.MailHandler(email[0], email, server_addr=smtp_host,  
                                       format_string=u'''Subject: [BCBB pipeline] {record.extra[run]} \n\n {record.message}''',
                                       level='INFO', bubble = True)
     return handler
