79449e5b1e3764ec4136d2cc3c59b0831f3e91c7
Valentine Svensson <valentine.svensson@scilifelab.se>
2012-05-23 15:36:40 +0200

    Unnested a couple of try-except blocks in sequencing_report.py

--- nextgen/bcbio/google/sequencing_report.py
+++ nextgen/bcbio/google/sequencing_report.py
@@ -50,71 +50,71 @@ def create_report_on_gdocs(fc_date, fc_name, run_info_yaml, dirs, config):
         def record_processor(record):
             return record.extra.__setitem__('run', "%s_%s" % (fc_date, fc_name))
 
-        with log_handler.applicationbound(), logbook.Processor(record_processor):
-            try:
-                log.info("Started creating sequencing report on Google docs for %s_%s on %s" % (fc_date, fc_name, time.strftime("%x @ %X")))
-
-                # Get a flowcell object
-                fc = Flowcell(fc_name, fc_date, run_info, dirs.get("work", None))
-
-                # Get the GDocs demultiplex result file title
-                gdocs_dmplx_spreadsheet = gdocs.get("gdocs_dmplx_file", None)
-                # Get the GDocs QC file title
-                gdocs_qc_spreadsheet = gdocs.get("gdocs_qc_file", None)
-
-                # FIXME: Make the bc stuff use the Flowcell module
-                if gdocs_dmplx_spreadsheet is not None:
-                    # Upload the data
-                    success &= bc_metrics.write_run_report_to_gdocs(fc, fc_date, fc_name, gdocs_dmplx_spreadsheet, encoded_credentials)
-                else:
-                    log.warn("Could not find Google Docs demultiplex results file title in configuration. \
-                        No demultiplex counts were written to Google Docs for %s_%s" \
-                        % (fc_date, fc_name))
-
-                # Parse the QC metrics
-                try:
-                    qc = RTAQCMetrics(dirs.get("flowcell", None))
-                except:
-                    qc = None
-
-                if gdocs_qc_spreadsheet is not None and qc is not None:
-                    success &= qc_metrics.write_run_report_to_gdocs(fc, qc, gdocs_qc_spreadsheet, encoded_credentials)
-                else:
-                    log.warn("Could not find Google Docs QC file title in configuration. \
-                        No QC data were written to Google Docs for %s_%s" \
-                        % (fc_date, fc_name))
-
-                # Get the projects parent folder
-                projects_folder = gdocs.get("gdocs_projects_folder", None)
-
-                # Write the bc project summary report
-                if projects_folder is not None:
-                    success &= create_project_report_on_gdocs(fc, qc, \
-                        encoded_credentials, projects_folder)
-
-            except Exception as e:
-                success = False
-                raise
-
-            if success:
-                log.info("Sequencing report successfully created on \
-                    Google docs for %s_%s on %s" \
-                    % (fc_date, fc_name, time.strftime("%x @ %X")))
-            else:
-                log.warn("Encountered exception when writing sequencing \
-                    report for %s_%s to Google docs on %s" \
-                    % (fc_date, fc_name, time.strftime("%x @ %X")))
-
     except Exception as e:
         success = False
         log.warn("Encountered exception when writing sequencing report to Google Docs: %s" % e)
+
+    with log_handler.applicationbound(), logbook.Processor(record_processor):
+        try:
+            log.info("Started creating sequencing report on Google docs for %s_%s on %s" % (fc_date, fc_name, time.strftime("%x @ %X")))
+
+            # Get a flowcell object
+            fc = Flowcell(fc_name, fc_date, run_info, dirs.get("work", None))
+
+            # Get the GDocs demultiplex result file title
+            gdocs_dmplx_spreadsheet = gdocs.get("gdocs_dmplx_file", None)
+            # Get the GDocs QC file title
+            gdocs_qc_spreadsheet = gdocs.get("gdocs_qc_file", None)
+
+            # FIXME: Make the bc stuff use the Flowcell module
+            if gdocs_dmplx_spreadsheet is not None:
+                # Upload the data
+                success &= bc_metrics.write_run_report_to_gdocs(fc, fc_date, fc_name, gdocs_dmplx_spreadsheet, encoded_credentials)
+            else:
+                log.warn("Could not find Google Docs demultiplex results file title in configuration. \
+                    No demultiplex counts were written to Google Docs for %s_%s" \
+                    % (fc_date, fc_name))
+
+            # Parse the QC metrics
+            try:
+                qc = RTAQCMetrics(dirs.get("flowcell", None))
+            except:
+                qc = None
+
+            if gdocs_qc_spreadsheet is not None and qc is not None:
+                success &= qc_metrics.write_run_report_to_gdocs(fc, qc, gdocs_qc_spreadsheet, encoded_credentials)
+            else:
+                log.warn("Could not find Google Docs QC file title in configuration. \
+                    No QC data were written to Google Docs for %s_%s" \
+                    % (fc_date, fc_name))
+
+            # Get the projects parent folder
+            projects_folder = gdocs.get("gdocs_projects_folder", None)
+
+            # Write the bc project summary report
+            if projects_folder is not None:
+                success &= create_project_report_on_gdocs(fc, qc, \
+                    encoded_credentials, projects_folder)
+
+        except Exception as e:
+            success = False
+            log.warn("Encountered exception when writing sequencing report to Google Docs: %s" % e)
+
+        if success:
+            log.info("Sequencing report successfully created on \
+                Google docs for %s_%s on %s" \
+                % (fc_date, fc_name, time.strftime("%x @ %X")))
+        else:
+            log.warn("Encountered exception when writing sequencing \
+                report for %s_%s to Google docs on %s" \
+                % (fc_date, fc_name, time.strftime("%x @ %X")))
 
     return success
 
 
 def create_project_report_on_gdocs(fc, qc, encoded_credentials, gdocs_folder):
-    """Upload the sample read distribution for a project to google docs"""
-
+    """Upload the sample read distribution for a project to google docs.
+    """
     success = True
 
     # Create a client class which will make HTTP requests with Google Docs server.
