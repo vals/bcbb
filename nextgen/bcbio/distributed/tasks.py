c4777e24e1b9e8862b41b65f932207ee186ed277
Valentine Svensson <valentine.svensson@scilifelab.se>
2012-08-21 10:34:33 +0200

    Added parallel call to mark_duplicates_sample() in automated_initial_analysis. As well as the parallellization tasks. The function will execute if the field algorithm: mark_duplicates is to True in the config.

--- nextgen/bcbio/distributed/tasks.py
+++ nextgen/bcbio/distributed/tasks.py
@@ -22,23 +22,26 @@ def analyze_and_upload(*args):
     remote_info = args[0]
     toplevel.analyze_and_upload(remote_info, config_file)
 
+
 @task(ignore_results=True, queue="toplevel")
 def fetch_data(*args):
     """Transfer sequencing data from a remote machine. Could be e.g. a sequencer
-       or a pre-processing machine. 
+    or a pre-processing machine.
     """
     config_file = celeryconfig.BCBIO_CONFIG_FILE
     remote_info = args[0]
     toplevel.fetch_data(remote_info, config_file)
 
+
 @task(ignore_results=True, queue="toplevel")
 def backup_data(*args):
     """Backup sequencing data from a remote machine. Could be e.g. a sequencer
-       or a pre-processing machine. 
+    or a pre-processing machine.
     """
     config_file = celeryconfig.BCBIO_CONFIG_FILE
     remote_info = args[0]
     toplevel.backup_data(remote_info, config_file)
+
 
 @task(ignore_results=True, queue="storage")
 def long_term_storage(*args):
@@ -60,6 +63,11 @@ def remove_contaminants(*args):
 @task
 def process_alignment(*args):
     return lane.process_alignment(*args)
+
+
+@task
+def mark_duplicates_sample(*args):
+    return sample.mark_duplicates_sample(*args)
 
 
 @task
