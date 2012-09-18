34c82a7aa7c9c2011e68d3e22f37d7697e3eac6c
Guillermo Carrasco <guille.ch.88@gmail.com>
2012-09-18 14:26:07 +0200

    Adapted to run with automate deployment recipe

--- nextgen/tests/runtests_drmaa.py
+++ nextgen/tests/runtests_drmaa.py
@@ -12,20 +12,25 @@ def main():
 
     jt = s.createJobTemplate()
     jt.remoteCommand = 'nosetests'
-    jt.args = ['-v', '-s', '--with-xunit']
+    jt.args = ['-v', '-s', '--with-xunit', '-a', 'standard']
 
     jt.jobName = "testing_pipeline"
-    jt.workingDirectory = drmaa.JobTemplate.HOME_DIRECTORY+'opt/bcbb/nextgen/tests'
-    jt.outputPath = ":"+drmaa.JobTemplate.HOME_DIRECTORY+'/job_stdout.out'
-    jt.nativeSpecification = "-A a2010002 -p node -t 03:00:00"
+    jt.workingDirectory = drmaa.JobTemplate.HOME_DIRECTORY+'/opt/bcbb/nextgen/tests'
+    jt.outputPath = ":"+drmaa.JobTemplate.HOME_DIRECTORY+'/opt/bcbb/nextgen/tests/tests_results.out'
+    jt.nativeSpecification = "-A a2010002 -p node -t 04:00:00"
+    jt.email=["guillermo.carrasco@scilifelab.se"]
 
     jobid = s.runJob(jt)
     print 'Your job has been submitted with id ' + jobid
-    print 'The results are being written in ~/tests.out'
+    print 'The results are being written in ~/opt/bcbb/nextgen/tests/tests_results.out'
+    retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+    print 'Job: ' + str(retval.jobId) + ' finished with status ' + str(retval.hasExited)
 
+    print 'Cleaning up'
     s.deleteJobTemplate(jt)
     s.exit()
     exit(0)
 
 if __name__ == "__main__":
     main()
+
